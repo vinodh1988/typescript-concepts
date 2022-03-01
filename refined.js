de7929e3de7995840339894ace1326e49b2e4354
vinodh1988 <k.c.vinodh@hotmail.com>
2022-03-01 16:12:45 +0530

    type-code

--- refined.js
+++ refined.js
@@ -1,11 +1,11 @@
 function fun(a) {
-    a("store");
+    a("india");
     console.log("Finished the function");
 }
 function accept(a) {
-    var result = a(43);
+    var result = a(12);
     console.log("result is ", result);
 }
 accept(function (x) { return x % 2 == 0; });
-accept(function (x) { return x; });
+//accept(x=>x)
 fun(function (x) { return console.log("We are processing it ", x); });
--- refined.ts
+++ refined.ts
@@ -1,22 +1,24 @@
 interface Callback{
    (message:string):void
 }
-
+/*
 interface Predicate{
     (x:number):boolean
-}
+}*/
+
+type Predicate = (x:number)=>boolean  
 
 function fun(a:Callback){
-     a("store")
+     a("india")
      console.log("Finished the function")
 }
 
 function accept(a:Predicate){
-    let result=a(43)
+    let result=a(12)
     console.log("result is ",result)
 }
 
-accept(x=>x%2==0)
+accept((x)=>x%2==0)
 //accept(x=>x)
 
 fun(x=> console.log("We are processing it ",x))
