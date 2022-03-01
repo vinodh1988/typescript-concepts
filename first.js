819cf859c8f471869e7ba4ce9a73211b4ac1d473
vinodh1988 <k.c.vinodh@hotmail.com>
2022-03-01 14:30:57 +0530

    first-commit

---
+++ first.js
@@ -0,0 +1,9 @@
+var a = "raj";
+var b = 234;
+class Student {
+    constructor(sno, name) {
+        this.sno = sno;
+        this.name = name;
+    }
+}
+var one = new Student(12, "Raj");
---
+++ first.ts
@@ -0,0 +1,15 @@
+var a:string="raj";
+var b:number=234;
+
+class Student{
+    private sno:number;
+    private name:string;
+    
+    constructor(sno:number,name:string){
+        this.sno=sno;
+        this.name=name;
+    }
+}
+
+
+var one=new Student(12,"Raj")
